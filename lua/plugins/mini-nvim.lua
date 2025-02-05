97b1c113296ad2ddfeeeb3e23142364079a740b7
Emarpie <mrkrynp@gmail.com>
2025-02-06 00:38:44 +0800

    Modify quotations.

--- lua/plugins/mini-nvim.lua
+++ lua/plugins/mini-nvim.lua
@@ -1,22 +1,24 @@
 return {
-  'echasnovski/mini.nvim',
-  version = false,
-  config = function()
-    -- Better Around/Inside textobjects
-    --  - va)  - [V]isually select [A]round [)]paren
-    --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-    --  - ci'  - [C]hange [I]nside [']quote
-    require('mini.ai').setup { n_lines = 500 }
+	"echasnovski/mini.nvim",
+	version = false,
+	config = function()
+		-- Better Around/Inside textobjects
+		--  - va)  - [V]isually select [A]round [)]paren
+		--  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+		--  - ci'  - [C]hange [I]nside [']quote
+		require("mini.ai").setup({ n_lines = 500 })
 
-    -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
-    require('mini.move').setup()
+		-- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+		require("mini.move").setup()
 
-    require('mini.bracketed').setup()
+		require("mini.bracketed").setup()
 
-    require('mini.files').setup()
+		require("mini.files").setup()
 
-    require('mini.jump').setup()
+		require("mini.jump").setup()
 
-    require('mini.surround').setup()
-  end
+		require("mini.surround").setup()
+
+		require("mini.pairs").setup()
+	end,
 }
