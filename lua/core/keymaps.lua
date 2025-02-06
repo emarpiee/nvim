5fae2a482fb9435182a66f0c012fd3bbc8754258
Emarpie <mrkrynp@gmail.com>
2025-02-07 01:11:18 +0800

    Fromatted

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,54 +1,52 @@
--- -- -- -- MAIN KEYPMAPS -- -- -- --
---
+-- NOTE: -- -- MAIN KEYMAPS -- -- --
+
 -- Set leader key
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Disable the spacebar key's default behavior in Normal and Visual modes
+-- Disable spacebar in Normal and Visual modes
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 -- For conciseness
 local opts = { noremap = true, silent = true }
 
--- save file
-vim.keymap.set("n", "<C-s>", "<cmd> w <CR>", opts)
+-- NOTE: ## FILE OPERATIONS
 
--- save file without auto-formatting
-vim.keymap.set("n", "<leader>sn", "<cmd>noautocmd w <CR>", opts)
+-- Save file
+vim.keymap.set("n", "<C-s>", "<cmd>w<CR>", opts)
 
--- quit file
-vim.keymap.set("n", "<C-q>", "<cmd> q <CR>", opts)
+-- Save file without auto-formatting
+vim.keymap.set("n", "<leader>sn", "<cmd>noautocmd w<CR>", opts)
 
--- delete single character without copying into register
+-- Quit file
+vim.keymap.set("n", "<C-q>", "<cmd>q<CR>", opts)
+
+-- Delete single character without copying to register
 vim.keymap.set("n", "x", '"_x', opts)
+
+-- NOTE: ## SCROLLING
 
 -- Vertical scroll and center
 vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
 vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
 
 -- Find and center
 vim.keymap.set("n", "n", "nzzzv", opts)
 vim.keymap.set("n", "N", "Nzzzv", opts)
 
+-- NOTE: ## WINDOW MANAGEMENT
+
 -- Resize with arrows
-vim.keymap.set("n", "<Up>", ":resize -2<CR>", opts)
-vim.keymap.set("n", "<Down>", ":resize +2<CR>", opts)
-vim.keymap.set("n", "<Left>", ":vertical resize -2<CR>", opts)
-vim.keymap.set("n", "<Right>", ":vertical resize +2<CR>", opts)
+vim.keymap.set("n", "<C-Up>", ":resize -2<CR>", opts)
+vim.keymap.set("n", "<C-Down>", ":resize +2<CR>", opts)
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
--- Buffers
-vim.keymap.set("n", "<Tab>", ":bnext<CR>", opts)
-vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", opts)
-vim.keymap.set("n", "<leader>x", ":bdelete!<CR>", opts) -- close buffer
-vim.keymap.set("n", "<leader>b", "<cmd> enew <CR>", opts) -- new buffer
-
--- Window management
-vim.keymap.set("n", "<leader>v", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-vim.keymap.set("n", "<leader>h", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Resize splits equally" }) -- make split windows equal width & height
-vim.keymap.set("n", "<leader>xs", ":close<CR>", { desc = "Close split" }) -- close current split window
+-- Split windows
+vim.keymap.set("n", "<leader>v", "<C-w>v", { desc = "Split window vertically" })
+vim.keymap.set("n", "<leader>h", "<C-w>s", { desc = "Split window horizontally" })
+vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Resize splits equally" })
+vim.keymap.set("n", "<leader>xs", ":close<CR>", { desc = "Close split" })
 
 -- Navigate between splits
 vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", opts)
@@ -56,32 +54,43 @@ vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", opts)
 vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", opts)
 vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", opts)
 
+-- NOTE: ## BUFFER MANAGEMENT
+
+-- Buffer navigation
+vim.keymap.set("n", "<Tab>", ":bnext<CR>", opts)
+vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", opts)
+vim.keymap.set("n", "<leader>x", ":bdelete!<CR>", opts) -- Close buffer
+vim.keymap.set("n", "<leader>b", "<cmd>enew<CR>", opts) -- New buffer
+
 -- Tabs
-vim.keymap.set("n", "<leader>to", ":tabnew<CR>", opts) -- open new tab
-vim.keymap.set("n", "<leader>tx", ":tabclose<CR>", opts) -- close current tab
-vim.keymap.set("n", "<leader>tn", ":tabn<CR>", opts) --  go to next tab
-vim.keymap.set("n", "<leader>tp", ":tabp<CR>", opts) --  go to previous tab
+vim.keymap.set("n", "<leader>to", ":tabnew<CR>", opts) -- Open new tab
+vim.keymap.set("n", "<leader>tx", ":tabclose<CR>", opts) -- Close current tab
+vim.keymap.set("n", "<leader>tn", ":tabn<CR>", opts) -- Next tab
+vim.keymap.set("n", "<leader>tp", ":tabp<CR>", opts) -- Previous tab
+
+-- NOTE: ## MISCELLANEOUS
 
 -- Toggle line wrapping
 vim.keymap.set("n", "<leader>lw", "<cmd>set wrap!<CR>", opts)
 
--- Stay in indent mode
+-- Stay in indent mode after shifting
 vim.keymap.set("v", "<", "<gv", opts)
 vim.keymap.set("v", ">", ">gv", opts)
 
--- Keep last yanked when pasting
+-- Keep last yanked content when pasting
 vim.keymap.set("v", "p", '"_dP', opts)
 
--- Diagnostic keymaps
+-- NOTE: ## DIAGNOSTICS
+
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
 vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
--- Clear highlights on search when pressing <Esc> in normal mode
+-- Clear highlights on search when pressing <Esc>
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
--- Replace word (excluding symbols) under cursor across entire buffer
+-- Replace word under cursor across the buffer
 vim.keymap.set(
 	"n",
 	"<leader>ra",
@@ -89,20 +98,41 @@ vim.keymap.set(
 	{ desc = "Replace all words under cursor" }
 )
 
--- Jump to start line of the first line.
+-- Jump to first/last line of the file
 vim.keymap.set("n", "gg", "gg0")
-
--- JUmp to the end of the last line.
 vim.keymap.set("n", "G", "G$")
 
 -- Select all text in the file
 vim.api.nvim_set_keymap("n", "<C-a>", "gg0vG$", { noremap = true, silent = true })
 
--- Exit insert mode without hitting Esc
+-- Exit insert mode without pressing Esc
 vim.keymap.set("i", "jj", "<Esc>")
 
--- NOTE: # CUSTOM KEYMAPS FOR PLUGINS
---
+-- Open HTML in Browser (cross-platform) -- -- --
+
+vim.keymap.set({ "n", "v" }, "<leader>oh", function()
+	local filename = vim.fn.expand("%:t")
+	if filename:match("%.html$") or filename:match("%.htm$") then
+		local os_name = vim.loop.os_uname().sysname
+		if os_name == "Darwin" then
+			-- macOS
+			vim.cmd("silent !open %")
+		elseif os_name == "Linux" then
+			-- Linux
+			vim.cmd("silent !xdg-open %")
+		elseif os_name == "Windows_NT" then
+			-- Windows
+			vim.cmd("silent !start explorer %")
+		else
+			print("Unsupported OS.")
+		end
+	else
+		print("This is not an HTML file.")
+	end
+end, { desc = "Open HTML file in system browser" })
+
+-- NOTE: -- -- CUSTOM KEYMAPS FOR PLUGINS -- -- --
+
 -- mini.nvim
 vim.keymap.set("n", "<leader>e", "<Cmd>lua MiniFiles.open()<CR>", { desc = "Open MiniFiles" })
 
@@ -110,4 +140,4 @@ vim.keymap.set("n", "<leader>e", "<Cmd>lua MiniFiles.open()<CR>", { desc = "Open
 vim.keymap.set("n", "<leader>sp", "<Cmd>lua Snacks.picker()<CR>", { desc = "Open MiniuPicker" })
 
 -- mason.nvim
-vim.keymap.set("n", "<leader>Cm", "<Cmd>checkhealth mason <CR>", { desc = "Checkhealth Mason" })
+vim.keymap.set("n", "<leader>Cm", "<Cmd>checkhealth mason<CR>", { desc = "Checkhealth Mason" })
