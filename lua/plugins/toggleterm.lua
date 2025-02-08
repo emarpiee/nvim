6981ede096e8022f40380473406c222efd8ed90d
Emarpie <mrkrynp@gmail.com>
2025-02-09 01:53:15 +0800

    Formatted

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,37 +1,34 @@
 return {
-  'akinsho/toggleterm.nvim',
-  version = "*",
-  config = function()
-    require('toggleterm').setup({
-      direction = 'float',
-      close_on_exit = true,
-      float_opts = {
-        border = 'curved', -- 'single' | 'double' | 'shadow' | 'curved'
-        width = 120,
-        height = 40,
-      },
-      -- Create custom terminal commands (e.g., lazygit)
-      terminals = {
-        lazygit = {
-          cmd = "lazygit",
-          direction = 'float',
-          hidden = true
-        }
-      }
-    })
-    -- Example custom keybinds:
-    -- Open terminal in horizontal split
-    vim.keymap.set('n', '<leader>th', '<cmd>ToggleTerm direction=horizontal<CR>', { desc = 'Open horizontal terminal' })
-    -- Open terminal in vertical split
-    vim.keymap.set('n', '<leader>tv', '<cmd>ToggleTerm direction=vertical<CR>', { desc = 'Open vertical terminal' })
-    -- Open floating terminal
-    vim.keymap.set('n', '<leader>tf', '<cmd>ToggleTerm direction=float<CR>', { desc = 'Open floating terminal' })
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	config = function()
+		require("toggleterm").setup({
+			direction = "float",
+			close_on_exit = true,
+			float_opts = {
+				border = "curved", -- 'single' | 'double' | 'shadow' | 'curved'
+				width = 120,
+				height = 40,
+			},
+			-- Create custom terminal commands (e.g., lazygit)
+			terminals = {
+				lazygit = {
+					cmd = "lazygit",
+					direction = "float",
+					hidden = true,
+				},
+			},
+		})
+    vim.keymap.set("n", "<leader>t", "", {desc = "Terminal"})
+		vim.keymap.set("n", "<leader>th", ":ToggleTerm direction=horizontal<CR>", { desc = "Horizontal Terminal" })
+		vim.keymap.set("n", "<leader>tv", ":ToggleTerm direction=vertical<CR>", { desc = "Vertical Terminal" })
+		vim.keymap.set("n", "<leader>tf", ":ToggleTerm direction=float<CR>", { desc = "Floating Terminal" })
 
-    -- Custom terminal launcher example (lazygit)
-    vim.keymap.set('n', '<leader>gg', function()
-      local Terminal = require('toggleterm.terminal').Terminal
-      local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
-      lazygit:toggle()
-    end, { desc = 'Open lazygit' })
-  end
+		-- Custom terminal launcher example (lazygit)
+		vim.keymap.set("n", "<leader>gg", function()
+			local Terminal = require("toggleterm.terminal").Terminal
+			local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
+			lazygit:toggle()
+		end, { desc = "Open lazygit" })
+	end,
 }
