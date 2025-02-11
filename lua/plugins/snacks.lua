b8443d53f4a2ecf3d301db2123613f5acb3ce6bb
Emarpie <mrkrynp@gmail.com>
2025-02-11 17:53:53 +0800

    Removed.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -52,7 +52,7 @@ return {
 			sections = {
 				{ section = "header" },
 				{ section = "keys", gap = 1, padding = 1 },
-				{ pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+				{ pane = 2, icon = " ", title = "Recent Files", indent = 2, padding = 1 },
 				{ section = "startup" },
 			},
 		},
@@ -62,7 +62,18 @@ return {
 			enabled = true,
 			timeout = 3000,
 		},
-		picker = { enabled = true },
+		picker = {
+      enabled = true,
+      sources = {
+        files = { hidden = true,
+        },
+        explorer = {
+          layout = {
+            layout = { position = "right" },
+          }
+        }
+      }
+    },
 		quickfile = { enabled = true },
 		scroll = { enabled = false },
 		statuscolumn = { enabled = false },
@@ -73,183 +84,63 @@ return {
 			},
 		},
 	},
-	keys = {
-		{
-			"<leader>|",
-			function()
-				Snacks.explorer({ layout = { preview = true } })
-			end,
-			desc = "Toggle explorer",
-		},
-		{
-			"<leader>ff",
-			function()
-				Snacks.picker.files()
-			end,
-			desc = "Find file",
-		},
-		{
-			"<leader>fr",
-			function()
-				Snacks.picker.recent()
-			end,
-			desc = "Recent files",
-		},
-		{
-			"<leader>fR",
-			function()
-				Snacks.picker.recent({ filter = { cwd = true } })
-			end,
-			desc = "Recent files (cwd)",
-		},
-		{
-			"<leader>fk",
-			function()
-				Snacks.picker.keymaps()
-			end,
-			desc = "Find keymaps",
-		},
-		{
-			"<leader>:",
-			function()
-				Snacks.picker.command_history()
-			end,
-			desc = "Command history",
-		},
-		{
-			"<leader>z",
-			function()
-				Snacks.zen()
-			end,
-			desc = "Toggle Zen Mode",
-		},
-		{
-			"<leader>Z",
-			function()
-				Snacks.zen.zoom()
-			end,
-			desc = "Toggle Zoom",
-		},
-		{
-			"<leader>.",
-			function()
-				Snacks.scratch()
-			end,
-			desc = "Toggle Scratch Buffer",
-		},
-		{
-			"<leader>S",
-			function()
-				Snacks.scratch.select()
-			end,
-			desc = "Select Scratch Buffer",
-		},
-		{
-			"<leader>H",
-			function()
-				Snacks.notifier.show_history()
-			end,
-			desc = "Notification History",
-		},
-		{
-			"<leader>fC",
-			function()
-				Snacks.rename.rename_file()
-			end,
-			desc = "Rename File",
-		},
-		{
-			"<leader>gB",
-			function()
-				Snacks.gitbrowse()
-			end,
-			desc = "Git Browse",
-			mode = { "n", "v" },
-		},
-		{
-			"<leader>gb",
-			function()
-				Snacks.git.blame_line()
-			end,
-			desc = "Git Blame Line",
-		},
-		{
-			"<leader>gf",
-			function()
-				Snacks.lazygit.log_file()
-			end,
-			desc = "Lazygit Current File History",
-		},
-		{
-			"<leader>gg",
-			function()
-				Snacks.lazygit()
-			end,
-			desc = "Lazygit",
-		},
-		{
-			"<leader>gl",
-			function()
-				Snacks.lazygit.log()
-			end,
-			desc = "Lazygit Log (cwd)",
-		},
-		{
-			"<leader>un",
-			function()
-				Snacks.notifier.hide()
-			end,
-			desc = "Dismiss All Notifications",
-		},
-		{
-			"]]",
-			function()
-				Snacks.words.jump(vim.v.count1)
-			end,
-			desc = "Next Reference",
-			mode = { "n", "t" },
-		},
-		{
-			"[[",
-			function()
-				Snacks.words.jump(-vim.v.count1)
-			end,
-			desc = "Prev Reference",
-			mode = { "n", "t" },
-		},
-		{
-			"<leader>N",
-			desc = "Neovim News",
-			function()
-				Snacks.win({
-					file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
-					width = 0.75,
-					height = 0.8,
-					wo = {
-						spell = false,
-						wrap = true,
-						signcolumn = "yes",
-						statuscolumn = " ",
-						conceallevel = 3,
-					},
-				})
-			end,
-		},
-	},
+  keys = {
+    -- Explorer & File Pickers
+    { "<leader>|",  function() Snacks.explorer({ layout = { preview = true } }) end, desc = "Toggle Explorer" },
+    { "<leader>ff", function() Snacks.picker.files() end, desc = "Find File" },
+    { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent Files" },
+    { "<leader>fR", function() Snacks.picker.recent({ filter = { cwd = true } }) end, desc = "Recent Files (cwd)" },
+    { "<leader>fk", function() Snacks.picker.keymaps() end, desc = "Find Keymaps" },
+    { "<leader>:",  function() Snacks.picker.command_history() end, desc = "Command History" },
 
+    -- Zen Mode & Buffers
+    { "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
+    { "<leader>Z",  function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
+    { "<leader>bs", function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+    { "<leader>bS", function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+
+    -- Notifications
+    { "<leader>H",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
+    { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
+
+    -- File Operations
+    { "<leader>fC", function() Snacks.rename.rename_file() end, desc = "Rename File" },
+
+    -- Git Operations
+    { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse", mode = { "n", "v" } },
+    { "<leader>gb", function() Snacks.git.blame_line() end, desc = "Git Blame Line" },
+    { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit File History" },
+    { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
+    { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Lazygit Log (cwd)" },
+
+    -- Word Navigation
+    { "]]", function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
+    { "[[", function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
+
+    -- Neovim News
+    {
+        "<leader>N",
+        function()
+            Snacks.win({
+                file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+                width = 0.75,
+                height = 0.8,
+                wo = {
+                    spell = false,
+                    wrap = true,
+                    signcolumn = "yes",
+                    statuscolumn = " ",
+                    conceallevel = 3,
+                },
+            })
+        end,
+        desc = "Neovim News",
+    },
+  },
 	init = function()
 		vim.api.nvim_create_autocmd("User", {
 			pattern = "VeryLazy",
 			callback = function()
-				-- Setup some globals for debugging (lazy-loaded)
-				_G.dd = function(...)
-					Snacks.debug.inspect(...)
-				end
-				_G.bt = function()
-					Snacks.debug.backtrace()
-				end
-				vim.print = _G.dd -- Override print to use snacks for `:=` command
-
 				-- Create some toggle mappings
 				Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
 				Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
